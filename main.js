a38fa2a39324a5746ad51daaa5d262b373c6a279
Alicia Poindexter <alcampbel@gmail.com>
2015-10-30 23:28:43 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(init)
+
+function(init) {
+  $('.tower').on('click', diskSelect);
+}
+
+function(diskSelect) {
+  $(this).children('.disk:first-child').addClass('selected');
+}
+
+function(diskMove) {
+  if ()
+}
