a3c1b27059c864f0981bea8446c00359e6f7a450
Alicia Poindexter <alcampbel@gmail.com>
2015-10-31 08:39:11 -0700

    Select top disk

--- main.js
+++ main.js
@@ -11,24 +11,21 @@ var clickedTower;
 function thisTurn() {
   clickedTower = '#' + $(this).attr('id');
   diskSelect();
-  //debugger;
 }
 
 
 function diskSelect() {
   $selectedDisk = $(clickedTower).children('.disk:first-child')
   $selectedDisk.addClass('selected');
-  $chosenDisk = $('.selected').detach();
   //debugger;
 }
 
+function moveDisk() {
+  
+}
+
 // function diskDrop() {
-//   if ($chosenDisk) {
-//     $chosenDisk.appendTo(this);
-//   } else {
-//     diskSelect();
-//   }
-// }
+//   $('selected').appendTo(clickedTower);
   
    // if (selectedDisk.hasClass('selected')) {
   //   ('.selected').detach();
